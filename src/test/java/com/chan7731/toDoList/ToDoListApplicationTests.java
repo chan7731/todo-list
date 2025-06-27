f5afa35a7906a302dfa3e63be479f3d3af39a3f2
박찬혁 <cksgurrhtn@naver.com>
2025-06-27 16:37:47 +0900

    commit 1

---
+++ src/test/java/com/chan7731/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan7731.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
